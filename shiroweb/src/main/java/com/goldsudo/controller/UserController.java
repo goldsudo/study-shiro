bc17d4ee566a8e1d449e9dc3613b83b088a2b347
goldsudo <goldsudo@gmail.com>
2018-08-25 14:59:35 +0800

    通过shiro的注解的方式进行url角色过滤

--- shiroweb/src/main/java/com/goldsudo/controller/UserController.java
+++ shiroweb/src/main/java/com/goldsudo/controller/UserController.java
@@ -4,6 +4,8 @@ import com.goldsudo.entity.User;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserController {
 
-    @RequestMapping(value = "/login.do", method = RequestMethod.POST,produces = "application/json;charset=utf-8")
+    @RequestMapping(value = "/login.do", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
     @ResponseBody
     public String login(User user) {
         Subject subject = SecurityUtils.getSubject();
@@ -24,9 +26,23 @@ public class UserController {
         } catch (AuthenticationException e) {
             return e.getMessage();
         }
-        if(subject.hasRole("root")){
+        if (subject.hasRole("root")) {
             return "登陆成功，具有root权限";
         }
         return "登陆成功，无root权限";
+    }
+
+    @RequiresRoles(value={"root","admin"},logical = Logical.OR)
+    @RequestMapping(value = "/rootRole.do", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String rootRole() {
+        return "这是具有root或admin权限才能访问的api";
+    }
+
+    @RequiresRoles("user")
+    @RequestMapping(value = "/userRole.do", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String userRole() {
+        return "这是具有user权限才能访问的api";
     }
 }
